3654fad56e985ccf82d45dd83a6b1b06bd9be0bc
emilelmarsson <emil.elmarsson@gmail.com>
2019-02-28 20:36:41 +0100

    Edvin fix plos

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,47 @@ public class Main {
   }
 
   public Main() {
-    getInput();
+      getInput();
+
+      Random random = new Random();
+      int photoIndex = rand.nextInt(collection.size());
+      Photo firstPhoto = collection.get(photoIndex);
+      collection.remove(photoIndex);
+      boolean currentOrientation = firstPhoto.isHorizontal();
+
+      if (currentOrientation) {
+          slideShow.add(new Slide(firstPhoto, null));
+      }else {
+          for(int i=0; i<collection.size(); i++){
+              Photo currPhoto = collection.get(i);
+              if (!currPhoto.isHorizontal()) {
+                  slideShow.add(new Slide(firstPhoto, currPhoto));
+              }
+          }
+      }
+
+      for(int i=0; i<collection.size(); i++){
+          Slide currentSlide = slideShow.get(slideShow.size() - 1);
+          Photo nextPhoto = collection.get(i);
+          if (nextPhoto.isHorizontal()) {
+              Slide newSlide = new Slide(nextPhoto, null);
+              if (getTransitionScore(currentSlide, newSlide) >= 1) {
+                  slideShow.add(newSlide);
+              }
+          }else {
+              for (int j = i; j < collection.size(); j++) {
+                  Photo maybeVertical = collection.get(j);
+                  if (!maybeVertical.isHorizontal()){
+                    Slide newSlide = new Slide(nextPhoto, maybeVertical);
+                    if (getTransitionScore(currentSlide, newSlide) >= 1) {
+                        slideShow.add(newSlide);
+                    }
+
+                  }
+              }
+          }
+      }
+      printOutput();
   }
 
   public void getInput() {
